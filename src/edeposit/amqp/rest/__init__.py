6fafa16c70b7849aea315cd283c660ef4a8f82dc
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-09 13:24:41 +0200

    Added standard __init__.py template.

--- src/edeposit/amqp/rest/__init__.py
+++ src/edeposit/amqp/rest/__init__.py
@@ -1,1 +1,40 @@
-__import__('pkg_resources').declare_namespace(__name__)
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+
+
+# Functions & classes =========================================================
+def _instanceof(instance, cls):
+    """
+    Check type of `instance` by matching ``.__name__`` with `cls.__name__`.
+    """
+    return type(instance).__name__ == cls.__name__
+
+
+# Main function ===============================================================
+def reactToAMQPMessage(message, send_back):
+    """
+    React to given (AMQP) message. `message` is expected to be
+    :py:func:`collections.namedtuple` structure from :mod:`.structures` filled
+    with all necessary data.
+
+    Args:
+        message (object): One of the request objects defined in
+                          :mod:`.structures`.
+        send_back (fn reference): Reference to function for responding. This is
+                  useful for progress monitoring for example. Function takes
+                  one parameter, which may be response structure/namedtuple, or
+                  string or whatever would be normally returned.
+
+    Returns:
+        object: Response class from :mod:`structures`.
+
+    Raises:
+        ValueError: if bad type of `message` structure is given.
+    """
+
+
+    raise ValueError("'%s' is unknown type of request!" % str(type(message)))
