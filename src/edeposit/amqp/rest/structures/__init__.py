ce105d532c1b33b85cdcae90e269329abc85b33c
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-09 13:24:55 +0200

    Added sketch of the structures.

---
+++ src/edeposit/amqp/rest/structures/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from incomming import SaveLogin
+from incomming import RemoveLogin
+from incomming import StatusUpdate
+
+from outgoing import UploadRequest
---
+++ src/edeposit/amqp/rest/structures/incomming.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from collections import namedtuple
+
+
+# Variables ===================================================================
+# Functions & classes =========================================================
+class SaveLogin():
+    """
+    """
+
+
+class RemoveLogin():
+    """
+    """
+
+
+class StatusUpdate():
+    """
+    """
