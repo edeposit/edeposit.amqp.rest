fdd1f009c528353722129363e1e556cfcc6271a2
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-20 17:26:52 +0200

    #4: Added new class ZEOWrapper, which takes `server` and `port` parameters.

--- src/edeposit/amqp/rest/db_handler/__init__.py
+++ src/edeposit/amqp/rest/db_handler/__init__.py
@@ -4,5 +4,6 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from zeo_wrapper import ZEOWrapper
 from zeo_conf_wrapper import ZEOConfWrapper
 from zeo_wrapper_prototype import ZEOWrapperPrototype
