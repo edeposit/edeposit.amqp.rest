701dec2f31f8b170a884f88052f256b691187e38
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-20 17:28:09 +0200

    #5: Added tests of new ZEOWrapper class (#4).

--- tests/test_db_handler.py
+++ tests/test_db_handler.py
@@ -8,6 +8,7 @@ import pytest
 
 import transaction
 
+from rest.db_handler import ZEOWrapper
 from rest.db_handler import ZEOConfWrapper
 
 import environment_generator
@@ -29,17 +30,26 @@ def teardown_module(module):
 
 # Fixtures ====================================================================
 @pytest.fixture
-def zeo_wrapper():
+def zeo_conf_wrapper():
     return ZEOConfWrapper(
         conf_path=environment_generator.tmp_context_name("zeo_client.conf"),
         project_key=PROJECT_KEY,
     )
 
 
+@pytest.fixture
+def zeo_wrapper():
+    return ZEOWrapper(
+        server="localhost",
+        port=60985,
+        project_key=PROJECT_KEY,
+    )
+
+
 # Tests =======================================================================
-def test_storing_and_retreiving():
-    first_wrapper = zeo_wrapper()
-    second_wrapper = zeo_wrapper()
+def test_zeo_conf_wrapper_storing_and_retreiving():
+    first_wrapper = zeo_conf_wrapper()
+    second_wrapper = zeo_conf_wrapper()
 
     with transaction.manager:
         first_wrapper["something"] = "hello"
@@ -49,11 +59,26 @@ def test_storing_and_retreiving():
         assert second_wrapper["something"] == "hello"
 
 
-def test_storing(zeo_wrapper):
+def test_zeo_conf_wrapper_storing(zeo_conf_wrapper):
     with transaction.manager:
-        zeo_wrapper["azgabash"] = "hello"
+        zeo_conf_wrapper["azgabash"] = "hello"
 
 
-def test_retreiving(zeo_wrapper):
+def test_zeo_conf_wrapper_retreiving(zeo_conf_wrapper):
+    with transaction.manager:
+        assert zeo_conf_wrapper["azgabash"] == "hello"
+
+
+def test_zeo_wrapper_retreiving(zeo_wrapper):
     with transaction.manager:
         assert zeo_wrapper["azgabash"] == "hello"
+
+
+def test_zeo_wrapper_storing(zeo_wrapper):
+    with transaction.manager:
+        zeo_wrapper["zeo"] = "hello ZEO"
+
+
+def test_zeo_wrapper_retreiving_again(zeo_wrapper):
+    with transaction.manager:
+        assert zeo_wrapper["zeo"] == "hello ZEO"
