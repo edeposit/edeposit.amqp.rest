95f1b66d3778f5c5c3099482a8214df8fd24b3f0
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-21 17:45:35 +0200

    Lowlevel ZEO wrapper moved into own module.
    See https://github.com/Bystroushaak/zeo_connector for details.

--- src/edeposit/amqp/rest/db_handler/__init__.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Interpreter version: python 2.7
-#
-# Imports =====================================================================
-from zeo_wrapper import ZEOWrapper
-from zeo_conf_wrapper import ZEOConfWrapper
-from zeo_wrapper_prototype import ZEOWrapperPrototype
