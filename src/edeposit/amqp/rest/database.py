12099b96a8e4d00caa74f41a2d45b696b60f28ec
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-22 16:01:38 +0200

    Added part of the user management.

--- src/edeposit/amqp/rest/database.py
+++ src/edeposit/amqp/rest/database.py
@@ -4,31 +4,58 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from BTrees.OOBTree import OOBTree
+
+import transaction
 from zeo_connector import ZEOConfWrapper
 
 
 # Variables ===================================================================
-
-
-
 # Functions & classes =========================================================
+def create_hash(password):
+    return str(hash(password))  # TODO: change to cryptographic hashing function
+
+
 class DatabaseHandler(object):
     def __init__(self, conf_path, project_key):
         self.conf_path = conf_path
         self.project_key = project_key
 
+        self.users_key = "users"
+
         self.zeo = ZEOConfWrapper(conf_path, project_key)
 
+        # read the proper indexes
+        self.users = self._get_key_or_create(self.users_key)
+
+    def _get_key_or_create(self, key, obj_type=OOBTree):
+        with transaction.manager:
+            key_obj = self.zeo.get(key, None)
+
+            if key_obj is None:
+                key_obj = obj_type()
+                self.zeo[key] = key_obj
+
+            return key_obj
+
     def add_user(self, username, pw_hash):
-        pass
+        with transaction.manager:
+            self.users[username] = pw_hash
 
     def remove_user(self, username):
-        pass
+        with transaction.manager:
+            del self.users[username]
+
+    def is_valid_user(self, username, password, hashing_mechanism=create_hash):
+        with transaction.manager:
+            pass_hash = self.users.get(username, None)
+
+        if pass_hash is None:
+            return False
+
+        return create_hash(password) == pass_hash
 
     def save_status_update(self, status):
-        pass
-
-    def is_valid_user(self, username, password):
         pass
 
     def query_status(self, book_id):
