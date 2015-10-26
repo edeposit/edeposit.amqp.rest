569abc99f698c2efe775be6dbc38a99dfe284212
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-26 16:26:20 +0100

    Added docstrings. Added .is_valid_user_hashed().

--- src/edeposit/amqp/rest/database.py
+++ src/edeposit/amqp/rest/database.py
@@ -17,6 +17,14 @@ def create_hash(password):
 
 
 class DatabaseHandler(object):
+    """
+    Define interfaces to the database, configuration and so on.
+
+    Attributes:
+        conf_path (str): Path to the ZEO client XML configuration.
+        project_key (str): Project key, which is used to access ZEO.
+        zeo (obj): :class:`.ZEOConfWrapper` database object.
+    """
     def __init__(self, conf_path, project_key):
         self.conf_path = conf_path
         self.project_key = project_key
@@ -35,6 +43,13 @@ class DatabaseHandler(object):
 
 
 class UserHandler(DatabaseHandler):
+    """
+    User database.
+
+    Attributes:
+        users_key (str): Key which is used to access database.
+        users (obj): Dict-like object in ZEO.
+    """
     def __init__(self, conf_path, project_key):
         super(self.__class__, self).__init__(
             conf_path=conf_path,
@@ -46,21 +61,63 @@ class UserHandler(DatabaseHandler):
         self.users = self._get_key_or_create(self.users_key)
 
     def add_user(self, username, pw_hash):
+        """
+        Add new user / update user's details.
+
+        Args:
+            username (str): Username of the new user.
+            pw_hash (str): Hash of the user'r password. See :func:`create_hash`
+                for details.
+        """
         with transaction.manager:
             self.users[username] = pw_hash
 
     def remove_user(self, username):
+        """
+        Remove `username` from the database.
+
+        Args:
+            username (str): Username of the new user.
+        """
         with transaction.manager:
             del self.users[username]
 
-    def is_valid_user(self, username, password, hashing_mechanism=create_hash):
-        with transaction.manager:
-            pass_hash = self.users.get(username, None)
+    def is_valid_user_hashed(self, username, hashed_password):
+        """
+        Check whether `username` and user's `hashed_password` is valid.
 
-        if pass_hash is None:
+        Args:
+            username (str): Username.
+            hashed_password (str): Cleantext version of the password.
+
+        Returns:
+            bool: True if valid.
+        """
+        with transaction.manager:
+            stored_pass_hash = self.users.get(username, None)
+
+        if stored_pass_hash is None:
             return False
 
-        return create_hash(password) == pass_hash
+        return hashed_password == stored_pass_hash
+
+    def is_valid_user(self, username, password, hashing_mechanism=create_hash):
+        """
+        Check whether the username:password pair is valid or not.
+
+        Args:
+            username (str): Username.
+            password (str): Cleantext version of the password.
+            hashing_mechanism (fn ref): Hashing function. Default
+                :func:`create_hash`.
+
+        Returns:
+            bool: True if valid.
+        """
+        return self.is_valid_user_hashed(
+            username=username,
+            hashed_password=hashing_mechanism(password)
+        )
 
 
 class StatusHandler(DatabaseHandler):
