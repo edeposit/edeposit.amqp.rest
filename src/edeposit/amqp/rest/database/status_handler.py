bf23f6750a6b34268cea009766111ec5dbcf6a86
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-30 17:04:36 +0100

    #12: Added a lot of parst of status tracking database.

--- src/edeposit/amqp/rest/database/status_handler.py
+++ src/edeposit/amqp/rest/database/status_handler.py
@@ -5,20 +5,47 @@
 #
 # Imports =====================================================================
 import transaction
+from persistent import Persistent
 
 from database_handler import DatabaseHandler
 
 
 # Functions & classes =========================================================
 class SatusMessage(Persistent):
-    timestamp
-    message
+    def __init__(self, message, timestamp):
+        self.message = message.strip()
+        self.timestamp = timestamp
+
+    def __eq__(self, obj):
+        return self.message == obj.message and self.timestamp == obj.timestamp
+
+    def __ne__(self, obj):
+        return not self.__eq__(obj)
+
+    def __hash__(self):
+        return hash("%d%s" % (self.timestamp, self.message))
+
+    def __cmp__(self, obj):
+        return self.timestamp.__cmp__(self, obj.timestamp)
 
 
 class StatusInfo(Persistent):
-    pub_url
-    registered_ts
-    messages
+    def __init__(self, rest_id, pub_url, registered_ts):
+        self.rest_id = rest_id
+        self.pub_url = pub_url
+        self.messages = set()
+        self.registered_ts = registered_ts
+
+    def add_status_message(self, status_message):
+        self.messages.add(status_message)
+
+    def add_message(self, message, timestamp):
+        self.add_status_message(
+            SatusMessage(message, timestamp)
+        )
+
+    def get_messages(self):
+        return sorted(self.messages, key=lambda x: x.timestamp)
 
 
 class StatusHandler(DatabaseHandler):
@@ -33,14 +60,41 @@ class StatusHandler(DatabaseHandler):
         self.status_db = self._get_key_or_create(self.status_db_key)
 
         self.id_to_username = {}
-        self.status_db
+        self.unregistered_statuses = {}
 
     def register_status_tracking(self, username, rest_id):
         with transaction.manager:
             self.id_to_username[rest_id] = username
 
-    def save_status_update(self, status_update):
-        pass
+    def save_status_update(self, rest_id, message, timestamp, pub_url=None):
+        with transaction.manager:
+            status_info_obj = self.status_db.get(rest_id, None)
+
+            if not status_info_obj:
+                return
+
+            if pub_url:
+                status_info_obj.pub_url = pub_url
+
+            status_info_obj.add_message(message, timestamp)
 
     def query_status(self, username, rest_id):
-        pass
+        with transaction.manager:
+            status_info_obj = self.status_db.get(rest_id, None)
+            db_username = self.id_to_username.get(username, None)
+
+        if not db_username:
+            raise IndexError(
+                "%s is not registered to receive status updates for %s" % (
+                    username,
+                    rest_id
+                )
+            )
+
+        if username != db_username:
+            raise ..
+
+        if not status_info_obj:
+            return []
+
+        return status_info_obj.get_messages()
