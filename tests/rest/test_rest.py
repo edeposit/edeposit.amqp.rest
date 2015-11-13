f3c77ce9f4e1ce293f4c335dc219c0762ba8898c
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-13 17:14:44 +0100

    #5, #17: File is now send in body in each test.

--- tests/rest/test_rest.py
+++ tests/rest/test_rest.py
@@ -99,6 +99,7 @@ def send_request(data):
         urlparse.urljoin(API_URL, "submit"),
         data={"json_metadata": json.dumps(data)},
         auth=HTTPBasicAuth('user', 'pass'),
+        files={'file': "Whatever"},
     )
 
 
