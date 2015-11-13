3f702a14cb4d472b44e865b539376b9d16eccd07
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-13 16:52:13 +0100

    #5, #17: Added example script for upload of the file.

---
+++ tests/example_upload.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import json
+
+import requests
+
+
+# Main program ================================================================
+if __name__ == '__main__':
+    metadata = {
+        "nazev": "Story of mighty azgabash",
+        "poradi_vydani": "1",
+        "misto_vydani": "Praha",
+        "rok_vydani": "1989",
+        "zpracovatel_zaznamu": "/me",
+        "nazev_souboru": "story_of_mighty_azgabash.pdf",
+    }
+
+    resp = requests.post(
+        "http://localhost:8080/api/v1/submit",
+        data={"json_data": json.dumps(metadata)},
+        auth=requests.auth.HTTPBasicAuth('user', 'pass'),
+        files={'file': open(__file__, 'rb')}
+    )
+    print resp.text
