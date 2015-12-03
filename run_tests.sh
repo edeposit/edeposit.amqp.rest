617cdc537ae9d087c516dad97963c49170d0c70d
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-03 10:24:28 +0100

    #5: Added forced kill of the processes of launched servers to tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env sh
 export PYTHONPATH="src/edeposit/amqp:$PYTHONPATH"
 
+kill -9 `ps aux | grep rest | grep edeposit | cut -d " " -f 3 | xargs` 2>/dev/null
+
 py.test tests $@
+
+kill -9 `ps aux | grep rest | grep edeposit | cut -d " " -f 3 | xargs` 2>/dev/null
