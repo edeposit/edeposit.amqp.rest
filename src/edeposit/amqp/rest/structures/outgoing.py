fe76b62e79621385b9a8430ca5a9cfe19d684ddf
Bystroushaak <bystrousak@kitakitsune.org>
2015-12-08 15:57:05 +0100

    #7: Added docstrings to structures/outgoing.py.

--- src/edeposit/amqp/rest/structures/outgoing.py
+++ src/edeposit/amqp/rest/structures/outgoing.py
@@ -3,17 +3,27 @@
 #
 # Interpreter version: python 2.7
 #
+"""
+Structures which are sent back to edeposit.
+"""
 # Imports =====================================================================
 from collections import namedtuple
 
 
-# Variables ===================================================================
 # Functions & classes =========================================================
 class UploadRequest(namedtuple("UploadRequest", ["username",
                                                  "rest_id",
                                                  "b64_data",
                                                  "metadata"])):
     """
+    User's upload request with following data:
+
+    Attributes:
+        username (str): User's handle in the REST system. See
+            :class:`.SaveLogin` and :class:`.RemoveLogin`.
+        rest_id (str): Unique identificator of the request.
+        b64_data (str): Content of the file packed as base64 data.
+        metadata (dict): Dictionary with metadata.
     """
 
 
